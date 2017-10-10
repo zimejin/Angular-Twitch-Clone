aa54bf2052830a464e7fa57e6f6a2a6f438af909
Zim_Ejin <echowords2000@yahoo.com>
2017-10-10 23:29:06 +0100

    more tweaks

--- src/app/main/video-list/video-list.component.ts
+++ src/app/main/video-list/video-list.component.ts
@@ -10,18 +10,23 @@ import { TwitchPlayerService } from '../../shared/services/twitch-player.service
 
 export class VideoListComponent {
     public query: any = 'videos/top?';
-    @Input() videoList; 
+    public Liked: boolean = false;
+
+    // tslint:disable-next-line:member-access
+    @Input() videoList;
 
     constructor(
         private twitchservice: TwitchApiService,
-        private twitchplayerservice: TwitchPlayerService
-    ) {}
+        private twitchplayerservice: TwitchPlayerService) {}
 
-   /* TODO - Write a Play-Video Function. Which takes Video-ID as a parameter to Initialize 
+   /* TODO - Write a Play-Video Function. Which takes Video-ID as a parameter to Initialize
    a similar function In the Video-Player-Service to load the Video by ID */
 
    public play(video: any): void {
-       this.twitchplayerservice.playVideo(video)
+       this.twitchplayerservice.playVideo(video);
    }
 
+   public appeal(video: any) {
+    this.Liked = !this.Liked;
+   }
 }
--- src/app/shared/services/twitch-player.service.ts
+++ src/app/shared/services/twitch-player.service.ts
@@ -8,12 +8,12 @@ export class TwitchPlayerService {
     public twitch_player;
     public width: string = '400';
     public height: string = '300';
+
+    @Output() public currentVideoText: EventEmitter<any> = new EventEmitter(true);
     private currentVideoId: string;
 
-    @Output() currentVideoText: EventEmitter<any> = new EventEmitter(true);
-
     constructor() {}
-    /* TODO write a function to initialize the twitch-player frame taking into 
+    /* TODO write a function to initialize the twitch-player frame taking into
     consideration the VIDEO-ID */
     public createPlayer(): void {
         let options = {
@@ -22,15 +22,19 @@ export class TwitchPlayerService {
             video:  null
         };
         let interval = setInterval(() => {
-            if ((typeof _window.Twitch !== 'undefined') && _window.Twitch && _window.YT.Player) {
+            // tslint:disable-next-line:max-line-length
+            if ((typeof _window.Twitch !== 'undefined') && _window.Twitch && _window.Twitch.Player) {
                 this.twitch_player = new Twitch.Player('twitch-player', options);
                 this.twitch_player.setMuted(true);
+                clearInterval(interval);
             }
-            clearInterval(interval);
         }, 100 );
     }
     /* TODO - create a Play-Video function that would load the video by ID //'v109010497'*/
     public playVideo(video: any) {
+        if (!this.twitch_player) {
+            console.log('Video Player is Loading');
+        }
         this.twitch_player.setVideo(video._id);
         this.currentVideoId = video._id;
         this.currentVideoText.emit(video.title);
